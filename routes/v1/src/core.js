4421ebed2a3f0482ef8ed0a3f690402d68399807
Jose Maria Garcia Garcia <xemagg95@gmail.com>
2018-11-15 12:47:21 +0100

    Test inyección dependencias (I)

--- routes/v1/src/core.js
+++ routes/v1/src/core.js
@@ -1,28 +1,31 @@
 /**
- * Abstract Detector function. It defines the common prototype
- * for future detectors
- * @function Detector
- * @param {string} id - Id of the detector. This id would be use in
- * HTTP request.
- * @param {string} category - Category of the detector. Values can be
- * 'face', 'voice','body', 'physio' and 'others'.
- * @param {boolean} realTime - Boolean indicating if the API works in realTime.
- * @param {string} url - Url to which the request will be sent.
+ *
+ * @param {string} id - Name of the detector.
+ * @param {string} category
+ * @param {boolean} realTime
+ * @param {string} url
+ * @param {Object} otherOptions
+ * @param {Function} initialize
+ * @param {Function} extractEmotions
+ * @param {Function} translateToPAD
+ * @return {Object} New Detector object fully built
  */
-function Detector( id, category, realTime, url ) {
-	this.id = id;
-	this.category = category;
-	this.realTime = realTime;
-	this.url = url;
+function createDetector(
+	id,
+	category,
+	realTime,
+	url,
+	otherOptions,
+	initialize,
+	extractEmotions,
+	translateToPAD ) {
+	const newDetector = {};
+	Detector.call( newDetector, id, category, realTime, url, otherOptions );
+	newDetector.prototype = Object.create( Detector.prototype );
+	newDetector.prototype.initialize = initialize;
+	newDetector.prototype.extractEmotions = extractEmotions;
+	newDetector.prototype.translateToPAD = translateToPAD;
+	return newDetector;
 }
-/** @function analyse
- *  @param {Object} media - Media to analyse. This media will be sent
- * to the Detector's API
- */
-Detector.prototype.analyse = function( media ) {
-	console.log( 'Analyse method in Detector class' );
-};
 
-Detector.prototype.translateToPAD = function( results ) {
-	console.log( 'Translate method in Detector class' );
-};
+//createDetector( 'azure', 'face', false, 'wwww', () => {}, () => {} );
---
+++ routes/v1/src/detector.js
@@ -0,0 +1,31 @@
+/**
+ * Abstract Detector function. It defines the common prototype
+ * for future detectors
+ * @function Detector
+ * @param {string} id - Id of the detector. This id would be use in
+ * HTTP request.
+ * @param {string} category - Category of the detector. Values can be
+ * 'face', 'voice','body', 'physio' and 'others'.
+ * @param {boolean} realTime - Boolean indicating if the API works in realTime.
+ * @param {string} url - Url to which the request will be sent.
+ * @param {Object} otherOptions - Other options, such as API keys, and so on.
+ */
+function Detector( id, category, realTime, url, otherOptions ) {
+	this.id = id;
+	this.category = category;
+	this.realTime = realTime;
+	this.url = url;
+	this.otherOptions = otherOptions;
+}
+
+/** @function analyse
+ *  @param {Object} media - Media to analyse. This media will be sent
+ * to the Detector's API
+ */
+Detector.prototype.extractEmotions = function( media ) {
+	console.log( 'extractEmotions method in Detector class' );
+};
+
+Detector.prototype.translateToPAD = function( results ) {
+	console.log( 'Translate method in Detector class' );
+};
